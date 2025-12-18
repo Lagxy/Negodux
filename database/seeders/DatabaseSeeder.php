acdd2570f4f9e6a209555640dfe3add381815a9c
Zefr0xe <fmawla02@gmail.com>
2025-12-18 23:34:26 +0700

    fix: add UmkmSeeder and MentorSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            UmkmSeeder::class,
+            MentorSeeder::class,
+        ]);
     }
 }
