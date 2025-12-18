64d40e9042913a207e809ca432139f53e2931f11
Zefr0xe <fmawla02@gmail.com>
2025-12-19 01:11:55 +0700

    chore: cleanup cloud deployment files for shared hosting

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Update Apache port to match Railway provided PORT
-sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
-
-# Run the main container command
-exec docker-php-entrypoint apache2-foreground
